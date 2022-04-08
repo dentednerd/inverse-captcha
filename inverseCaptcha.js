83a4a021a2bc8f230a1f7889fbd63748315b7da1
Joey Imlay <jo.imlay@gmail.com>
2022-04-08 13:01:10 +0100

    refactors inverseCaptcha to chain reduces

--- inverseCaptcha.js
+++ inverseCaptcha.js
@@ -1,7 +1,7 @@
 const inverseCaptcha = (sequence) => {
   const digits = Array.from(String(sequence), num => Number(num));
 
-  const arrayOfSums = digits.reduce((array, currDigit, currIndex) => {
+  return digits.reduce((array, currDigit, currIndex) => {
     const isEqualToNextNum = currDigit === digits[currIndex + 1];
 
     const isLastNumAndIsEqualToFirstNum = currIndex === digits.length - 1 && currDigit === digits[0];
@@ -11,9 +11,8 @@ const inverseCaptcha = (sequence) => {
     }
 
     return array;
-  }, []);
-
-  return arrayOfSums.reduce((a, b) => a + b, 0);
+  }, [])
+  .reduce((a, b) => a + b, 0);
 }
 
 module.exports = { inverseCaptcha };
